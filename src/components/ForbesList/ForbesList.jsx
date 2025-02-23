30d2935f8922de660ec313f3df08157920c6452b
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-02-23 19:22:39 +0300

    add TransactionHistory

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,5 +1,30 @@
-const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import ForbesListItem from '../ForbesListItem/ForbesListItem';
+import style from './ForbesList.module.css';
+
+const ForbesList = ({ list }) => {
+  return (
+    <div className={style.board}>
+      <div className={style.header}>
+        <h2 className={style.title}>
+          <span className={style.titleTop}>Forbes</span>
+          <span className={style.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={style.list}>
+        {list.map(el => (
+          <li key={el.id} className={style.item}>
+            <ForbesListItem
+              avatar={el.avatar}
+              name={el.name}
+              capital={el.capital}
+              isIncrease={el.isIncrease}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ForbesList;
