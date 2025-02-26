d4c086092073012fe90e2a32ea359506a5061b70
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-02-26 18:35:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,28 @@ import Heading from './components/Heading/Heading';
 import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
 import forbes from './data/forbes.json';
-import { useState } from 'react';
-
-const CustomButton = ({ message, children }) => {
-  return <button onClick={() => alert(message)}>{children}</button>;
-};
+import { useState, useEffect } from 'react';
 
 export const App = () => {
   const [clicks, setClicks] = useState(0);
 
-  const handleClick = () => {
-    clicks = clicks + 1;
-  };
+  // ❌ Ефект записаний з помилкою!
+  useEffect(() => {
+    console.log('Clicks updated: ', clicks);
+  }, [clicks]);
   return (
     <Section>
       <Container>
-        <>
-          <CustomButton message="Playing music!">Play some music</CustomButton>
-          <CustomButton message="Uploading your data!">
-            Upload data
-          </CustomButton>
-        </>
+        <button onClick={() => setClicks(clicks + 1)}>
+          You clicked {clicks} times
+        </button>
 
         <Heading title="Task 1 Blog Card" bottom />
         <BlogCard />
-
         <Heading title="Task 2 Statistics" top bottom />
         <Statistics title="Main Statistics" />
-
         <Heading title="Task 3 Forbes list" top bottom />
         <ForbesList list={forbes} />
-
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
       </Container>
